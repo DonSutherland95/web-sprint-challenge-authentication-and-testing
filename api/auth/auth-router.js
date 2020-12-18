cf9f330475c6ce1909dfad7c05dea5d9a9172d3d
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-12-17 20:43:45 -0500

    push 4

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -122,7 +122,7 @@ function makeToken(user) {
 
 function validateUser(req, res, next) {
   if(!req.body.username || !req.body.password){
-    res.status(400).json({message: "username and password required"})
+    res.status(400).json("username and password required")
   } else{
     next()
   }
