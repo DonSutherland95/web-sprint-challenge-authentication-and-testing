394c9d32d56c7251b1df0299ac849b11e558e30c
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-12-17 19:54:12 -0500

    push 1

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,38 @@
 const router = require('express').Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../../config/secret')
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+const { isValid } = require("../users/users-service");
+const Users = require("../users/users-model");
+
+router.post('/register',validateUser, (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    // hash the password
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    // save the user to the database
+    Users.add(credentials)
+      .then(user => {
+        // console.log(user)
+        res.status(201).json({ data: user });
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "username taken",
+    });
+  }
+
+  // res.end('implement register, please!');
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -29,7 +60,30 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = makeToken(user);
+          res.status(200).json({
+            message: "Welcome to my API, " + user.username,
+            token,
+          });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
+  // res.end('implement login, please!');
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -54,5 +108,27 @@ router.post('/login', (req, res) => {
       the response body should include a string exactly as follows: "invalid credentials".
   */
 });
+function makeToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  }
+  const options = {
+    expiresIn: '900s',
+  }
+  return jwt.sign(payload, jwtSecret, options)
+}
+
+function validateUser(req, res, next) {
+  if(!req.body.username || !req.body.username){
+    res.status(400).json({message: "username and password required"})
+  } else{
+    next()
+  }
+  
+}
+function validateUserName(req, res, next){
+  if(req.body.username)
+}
 
 module.exports = router;
