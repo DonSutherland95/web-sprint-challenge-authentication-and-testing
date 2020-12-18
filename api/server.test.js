54966e296eaccdc6905027cf72721b92219fd0d5
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-12-17 21:39:35 -0500

    auth setup, tests passing and mvp completed

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,59 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+
+
+const request = require("supertest");
+const server = require('./server')
+const db = require('../data/dbConfig')
+const User = require('./users/users-model')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
 })
+beforeEach(async () => {
+  await db('users').truncate()
+})
+afterAll(async () => {
+  await db.destroy()
+})
+test('sanity', () => {
+  expect(true).toBe(true)
+})
+const user1 = {
+  username: "user1",
+  password: "pass1",
+};
+const user2 = {
+  username:"user2",
+  
+};
+
+describe("[POST] auth/register", () => {
+  it("Valid info check", async () => {
+    const res = await request(server)
+    .post("/api/auth/register")
+    .send(user1);
+    expect(res.body).toHaveProperty("username");
+    expect(res.body).toHaveProperty("password");
+  });
+  it("Invalid information check", async () => {
+    const res = await request(server)
+    .post("/api/auth/register")
+    .send(user2);
+    expect(res.body).toBe("username and password required");
+  });
+});
+
+describe("[POST] auth/login", () => {
+  it("Valid login check", async () => {
+    await request(server)
+    .post("/api/auth/register")
+    .send(user1);
+    const res = await request(server).post("/api/auth/login").send(user1);
+    expect(res.body).toHaveProperty("token");
+  });
+  it("Invalid login check", async () => {
+    const res = await request(server).post("/api/auth/login").send(user1);
+    expect(res.body).toHaveProperty("message");
+  });
+});
